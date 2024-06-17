1c216375fd8ae15fdc1c56e1a4648b45d2889cfa
mac <czakauddin@gmail.com>
2024-06-17 20:26:38 +0100

    Database files updates after every change

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const data = require('./data.json');
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +17,20 @@ contacts_array = data.map(
 );
 
 const book = new AddressBook(contacts_array);
+
+const update_file = () => {
+    const book_data = book.view_all().map((contact) => {
+        delete contact.index;
+        return contact;
+    })
+    fs.writeFile("data.json", JSON.stringify(book_data, null, 2), (error) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        console.log("data.json updated!");
+    });
+}
 
 app.get("/get_all_contacts", (req, res) => {
     res.status(200).json(book.view_all());
@@ -32,6 +47,7 @@ app.post("/add_contact", (req, res) => {
         book.add_contact(contact);
         const last_index = book.get_size() - 1;
         const temp =  book.get_contact(last_index);
+        update_file();
         res.status(201).json(temp);
     } else{
         res.status(206).json({"error" : "Incomplete details"});
@@ -42,6 +58,7 @@ app.delete("/delete_contact/:index", (req, res) => {
     const index = req.params.index;
     if(index < book.get_size()){
         book.delete_contact(index);
+        update_file();
         res.status(200).json({"message" : "Contact Deleted!"});
     } else {
         res.status(406).json({"error" : "Invalid Index!"});
@@ -54,6 +71,7 @@ app.put("/update_contact", (req, res) => {
         book.update_contact(body);
         const temp = book.get_contact(body.index);
         temp["message"] = "Contact Updated!";
+        update_file();
         res.status(200).json(temp);
     } else {
         res.status(406).json({"error" : "Invalid Index!"});
